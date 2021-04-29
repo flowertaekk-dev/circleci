5f4c57fa6a341f88cc9b233067f9ae334a1a18d8
flowertaekk-dev <flowertaekk.dev@gmail.com>
2021-04-29 16:04:00 +0900

    CICD test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ohoh
+	  Hello from circleCI
         </a>
       </header>
     </div>
